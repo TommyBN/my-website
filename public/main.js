91a917606f4184dce5ff9477c1f414c72a9d8199
Tom Ben Ner <tommybenner@hotmail.com>
2020-09-28 22:05:00 +0300

    developed welcome page, starting main page

--- public/main.js
+++ public/main.js
@@ -30,7 +30,7 @@ webpackEmptyAsyncContext.id = "./src/$$_lazy_route_resource lazy recursive";
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "p{\r\n    background-color: darksalmon;\r\n    color: red;\r\n}"
+module.exports = ""
 
 /***/ }),
 
@@ -41,7 +41,7 @@ module.exports = "p{\r\n    background-color: darksalmon;\r\n    color: red;\r\n
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "<div>\n  <h1>Hello, this is my website</h1>\n\n  <p>\n    My greatfull thanks to Elad Grieff, Code Master, who helped me upload this site.\n  </p>\n</div>\n\n"
+module.exports = "<router-outlet></router-outlet>"
 
 /***/ }),
 
@@ -94,6 +94,9 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _angular_platform_browser__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/platform-browser */ "./node_modules/@angular/platform-browser/fesm5/platform-browser.js");
 /* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
 /* harmony import */ var _app_component__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./app.component */ "./src/app/app.component.ts");
+/* harmony import */ var _welcome_welcome_component__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./welcome/welcome.component */ "./src/app/welcome/welcome.component.ts");
+/* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @angular/router */ "./node_modules/@angular/router/fesm5/router.js");
+/* harmony import */ var _main_main_component__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./main/main.component */ "./src/app/main/main.component.ts");
 var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -103,22 +106,243 @@ var __decorate = (undefined && undefined.__decorate) || function (decorators, ta
 
 
 
+
+
+
+var routes = [
+    { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+    { path: 'welcome', component: _welcome_welcome_component__WEBPACK_IMPORTED_MODULE_3__["WelcomeComponent"] },
+    { path: 'home', component: _main_main_component__WEBPACK_IMPORTED_MODULE_5__["MainComponent"] }
+];
 var AppModule = /** @class */ (function () {
     function AppModule() {
     }
     AppModule = __decorate([
         Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["NgModule"])({
             declarations: [
-                _app_component__WEBPACK_IMPORTED_MODULE_2__["AppComponent"]
+                _app_component__WEBPACK_IMPORTED_MODULE_2__["AppComponent"],
+                _welcome_welcome_component__WEBPACK_IMPORTED_MODULE_3__["WelcomeComponent"],
+                _main_main_component__WEBPACK_IMPORTED_MODULE_5__["MainComponent"]
             ],
             imports: [
-                _angular_platform_browser__WEBPACK_IMPORTED_MODULE_0__["BrowserModule"]
+                _angular_platform_browser__WEBPACK_IMPORTED_MODULE_0__["BrowserModule"],
+                _angular_router__WEBPACK_IMPORTED_MODULE_4__["RouterModule"].forRoot(routes)
             ],
             providers: [],
             bootstrap: [_app_component__WEBPACK_IMPORTED_MODULE_2__["AppComponent"]]
         })
     ], AppModule);
     return AppModule;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/main/main.component.css":
+/*!*****************************************!*\
+  !*** ./src/app/main/main.component.css ***!
+  \*****************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = ""
+
+/***/ }),
+
+/***/ "./src/app/main/main.component.html":
+/*!******************************************!*\
+  !*** ./src/app/main/main.component.html ***!
+  \******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "<p>\n  Hello {{name}}, welcome to my website\n</p>\n\n\n<h1>Nothing here yet. but soooooooooon to come :)</h1>"
+
+/***/ }),
+
+/***/ "./src/app/main/main.component.ts":
+/*!****************************************!*\
+  !*** ./src/app/main/main.component.ts ***!
+  \****************************************/
+/*! exports provided: MainComponent */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "MainComponent", function() { return MainComponent; });
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _user_service__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../user.service */ "./src/app/user.service.ts");
+var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (undefined && undefined.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+var MainComponent = /** @class */ (function () {
+    function MainComponent(userService) {
+        this.userService = userService;
+    }
+    MainComponent.prototype.ngOnInit = function () {
+        this.name = this.userService.userName;
+    };
+    MainComponent = __decorate([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["Component"])({
+            selector: 'app-main',
+            template: __webpack_require__(/*! ./main.component.html */ "./src/app/main/main.component.html"),
+            styles: [__webpack_require__(/*! ./main.component.css */ "./src/app/main/main.component.css")]
+        }),
+        __metadata("design:paramtypes", [_user_service__WEBPACK_IMPORTED_MODULE_1__["UserService"]])
+    ], MainComponent);
+    return MainComponent;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/user.service.ts":
+/*!*********************************!*\
+  !*** ./src/app/user.service.ts ***!
+  \*********************************/
+/*! exports provided: UserService */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "UserService", function() { return UserService; });
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (undefined && undefined.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var UserService = /** @class */ (function () {
+    function UserService() {
+    }
+    UserService = __decorate([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["Injectable"])({
+            providedIn: 'root'
+        }),
+        __metadata("design:paramtypes", [])
+    ], UserService);
+    return UserService;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/welcome/welcome.component.css":
+/*!***********************************************!*\
+  !*** ./src/app/welcome/welcome.component.css ***!
+  \***********************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "button {\r\n    border-radius: 5px;\r\n}\r\n\r\n.form-container {\r\n    width: 50%;\r\n    height: 30%;\r\n    padding: 5%;\r\n    margin-left: 40%;\r\n    \r\n    /* visibility: hidden; */\r\n}\r\n\r\n.form-container > div {\r\n    margin: 30px;\r\n}\r\n\r\n#hidden {\r\n    margin-left: 25%;\r\n}\r\n\r\n#hidden > h1 {\r\n    color: violet;\r\n}\r\n\r\n#codeMaster {\r\n    margin-left: 10%;\r\n}\r\n\r\n"
+
+/***/ }),
+
+/***/ "./src/app/welcome/welcome.component.html":
+/*!************************************************!*\
+  !*** ./src/app/welcome/welcome.component.html ***!
+  \************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "<!-- \n<div id=\"bar\">\n  <div id=\"logo\">\n    tommybn\n  </div>\n</div> -->\n\n\n<div class=\"container\">\n\n  \n  <div class=\"form-container\" [style.visibility]=\"isVisible() ? 'visible' : 'hidden'\">\n    \n    <div>\n      <h3>Your name please:</h3>\n    </div>\n    \n    <div>\n      <input type=\"text\" #name (click)=\"checkValidation()\" (keyup)=\"checkValidation()\">\n    </div>\n    \n    <div>\n      <button [disabled]=\"!formValid\" (click)=\"click(); showName=true\">OK, now what?</button>\n    </div>\n    \n    \n  </div>\n  \n  <div id=\"hidden\" [style.visibility]=\"isVisible() ? 'hidden' : 'visible'\" >\n    <h1> Hello {{myName}}!!!!!</h1>\n    <h3>Did you now that your Ascii-Numerologic number is {{hollyNumber}}?</h3>\n    <h4><a href=\"https://nameecho.com/ascii\">what is ascii??</a> </h4>\n    <h4> <a href=\"https://en.wikipedia.org/wiki/ASCII\">More about Ascii-Numerology</a>  </h4>\n    <h3> <a routerLink=\"/home\"> Fuck off, let me get inside tommy's website!</a></h3>\n  </div>\n  \n</div>\n\n\n\n\n\n\n\n\n\n<div id=\"codeMaster\">\n  <h2>Thanks to:</h2>\n    My greatfull thanks to Elad Grief, Code Master, who helped me upload this site.\n</div>\n\n"
+
+/***/ }),
+
+/***/ "./src/app/welcome/welcome.component.ts":
+/*!**********************************************!*\
+  !*** ./src/app/welcome/welcome.component.ts ***!
+  \**********************************************/
+/*! exports provided: WelcomeComponent */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "WelcomeComponent", function() { return WelcomeComponent; });
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _user_service__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../user.service */ "./src/app/user.service.ts");
+var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (undefined && undefined.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+var WelcomeComponent = /** @class */ (function () {
+    function WelcomeComponent(userService) {
+        this.userService = userService;
+        this.formValid = false;
+        this.showName = false;
+        this.hollyNumber = 0;
+    }
+    WelcomeComponent.prototype.ngOnInit = function () { };
+    WelcomeComponent.prototype.checkValidation = function () {
+        this.formValid = this.nameRef.nativeElement.value.length > 1 ? true : false;
+    };
+    WelcomeComponent.prototype.click = function () {
+        this.myName = this.nameRef.nativeElement.value;
+        this.userService.userName = this.myName;
+        this.calculateLetters();
+    };
+    WelcomeComponent.prototype.isVisible = function () {
+        if (!this.showName)
+            return true;
+        else
+            return false;
+    };
+    WelcomeComponent.prototype.calculateLetters = function () {
+        var tempNumber = 0;
+        for (var _i = 0, _a = this.myName; _i < _a.length; _i++) {
+            var letter = _a[_i];
+            tempNumber += letter.charCodeAt(0);
+        }
+        this.hollyNumber = this.digitalRoot(tempNumber);
+        console.log(this.hollyNumber);
+    };
+    WelcomeComponent.prototype.digitalRoot = function (number) {
+        var strin = number.toString();
+        var len = strin.length;
+        if (len == 1)
+            return number;
+        var sum = 0;
+        for (var i = 0; i < len; i++) {
+            sum += parseInt(strin[i]);
+        }
+        return this.digitalRoot(sum);
+    };
+    __decorate([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["ViewChild"])('name'),
+        __metadata("design:type", _angular_core__WEBPACK_IMPORTED_MODULE_0__["ElementRef"])
+    ], WelcomeComponent.prototype, "nameRef", void 0);
+    WelcomeComponent = __decorate([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["Component"])({
+            selector: 'app-welcome',
+            template: __webpack_require__(/*! ./welcome.component.html */ "./src/app/welcome/welcome.component.html"),
+            styles: [__webpack_require__(/*! ./welcome.component.css */ "./src/app/welcome/welcome.component.css")]
+        }),
+        __metadata("design:paramtypes", [_user_service__WEBPACK_IMPORTED_MODULE_1__["UserService"]])
+    ], WelcomeComponent);
+    return WelcomeComponent;
 }());
 
 
