920a8dfc0075ac300f575cb435ecbb3d846e3b0a
Tom Ben Ner <tommybenner@hotmail.com>
2020-10-15 17:21:50 +0300

    added db connection

--- app.js
+++ app.js
@@ -1,27 +1,34 @@
+const PORT = process.env.PORT || 5000
 var express = require('express');
+var app = express();
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-const PORT = process.env.PORT || 5000
-
-
-var app = express();
-
 const cors = require('cors')
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://127.0.0.1:27017/";
+
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser())
 
-app.get('/welcome', (req,res)=>{
-  res.redirect('/')
-})
-
-
 app.listen(PORT, () => console.log(`Listening on tommybn.com:${PORT}`))
 
-// app.use('/api/v1/users', users);
+app.get('/test', (req, res)=>{
+  res.send('ok, got to test')
+})
+
+MongoClient.connect(url, { useUnifiedTopology: true }, function (err, db) {
+    if (err) throw err;
+    console.log('connected to DB')
+    var dbo = db.db("mydb");
+    dbo.listCollections().toArray(function (err, collectionInfos) {
+        console.log(collectionInfos);
+        db.close();
+    });
+})
+
 
 module.exports = app;
