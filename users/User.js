98217ea64828d7dde5dc2c74b14c3fccc78d5573
Tom Ben Ner <tommybenner@hotmail.com>
2020-10-22 14:19:54 +0300

    added login and signup logics

--- users/User.js
+++ users/User.js
@@ -1,23 +1,33 @@
+
+
 class User {
 
     constructor(name, password) {
         this.name = name;
-        this.password = password
+        this.password = /**bcrypt */ password;
+        this.todos = [];
+        this.events = [];
+        this.jobPlaces = [];
     }
-
-    // set name(name) {
-    //     this.name = name.charAt(0).toUpperCase() + name.slice(1);
-    //   }
+    
     get name() {
         return this.name;
     }
 
-    sayHello() {
-        console.log('Hello, my name is ' + this.name + ', I have ID: ' + this.id);
+    set name(name) {
+        this.name = name;
     }
+
+    get password() {
+        return this.password;
+    }
+
+    // sayHello() {
+    //     console.log('Hello, my name is ' + this.name + ', I have ID: ' + this.id);
+    // }
 }
 
-//var justAGuy = new Person();
+//var justAGuy = new User(nbs,ma);
 //justAGuy.name = 'martin'; // The setter will be used automatically here.
 //justAGuy.sayHello(); // Will output 'Hello, my name is Martin, I have ID: id_1'
 
--- users/routes.js
+++ users/routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const User = require('./User');
+const bcrypt = require('bcryptjs');
 const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb://127.0.0.1:27017/";
 
-
+//get all users
 router.get('/', (req, res) => {
     MongoClient.connect(url, { useUnifiedTopology: true }, function (err, db) {
         if (err) throw err;
@@ -17,7 +18,7 @@ router.get('/', (req, res) => {
     });
 })
 
-
+//get user
 router.get('/:id', (req, res) => {
     MongoClient.connect(url, { useUnifiedTopology: true }, function (err, db) {
         if (err) throw err;
@@ -30,20 +31,89 @@ router.get('/:id', (req, res) => {
     });
 })
 
+//sign up
+router.post('/signup', async (req, res) => {
 
-// router.post('/new', (req, res) => {
-//   console.log('todo to be saved: ', req.body);
+    var response = {
+        signedUp: false,
+        msg: '',
+    }
 
-//   // a document instance
-//   var currentTodo = new Todo(req.body);
+    //check if username exist
+    await MongoClient.connect(url, { useUnifiedTopology: true }, function (err, db) {
+        if (err) throw err;
+        let dbo = db.db("tommybn");
+        dbo.collection("users").findOne({ name: req.body.username }, async function (err, user) {
+            if (user) {
+                response.msg = 'שם המשתמש כבר קיים במערכת. אנא בחר שם אחר';
+                res.json(response)
+            }
+            else {
+                //create user object
+                let user = {
+                    name: req.body.username,
+                    password: req.body.password
+                };
+                //hash password
+                bcrypt.genSalt(10, (err, salt) =>
+                    bcrypt.hash(req.body.password, salt, (err, hash) => {
+                        if (err) throw err;
+                        user.password = hash;
+                        //save user    
+                        dbo.collection("users").insertOne(user, function (err, result) {
+                            if (err) throw err;
+                            let username = result.ops[0].name;
+                            response.signedUp = true;
+                            response.msg = `משתמש חדש בשם ${username} נרשם בהצלחה למערכת`
+                            res.status(200).json(response);
+                        });
+                    }))
+            }
+        });
+    })
+})
 
-//   // save model to database
-//   currentTodo.save(function (err, todo) {
-//     if (err) return console.error(err);
-//     console.log('todo with ID ' + todo.id + " saved to todos collection.");
-//   });
+router.post('/login', (req, res) => {
 
-//   res.send("success")
-// });
+    var response = {
+        valid: false,
+        msg: '',
+        id: ''
+    }
+
+    MongoClient.connect(url, { useUnifiedTopology: true }, function (err, db) {
+        if (err) throw err;
+        let dbo = db.db("tommybn");
+
+        //find user by name
+        dbo.collection("users").findOne({name: req.body.username}, function (err, user) {
+            if (err) throw err;
+            if(user){
+                //user exist, check password
+                bcrypt.compare(req.body.password, user.password, (err, match) => {
+                    if(err) throw err;
+                    if(match) {
+                        //passwords match, return success message
+                        response.valid = true;
+                        response.msg = 'כניסה מוצלחת';
+                        response.id = user._id;
+                        res.json(response);
+                    }
+                    else {
+                        //password incorrect, return failure message
+                        response.msg = 'סיסמא שגויה.';
+                        res.json(response);
+                    }
+                })
+            }
+            else {
+                //user doesnt exist, return failure message  
+                response.msg = 'שם המשתמש לא קיים במערכת.';
+                res.json(response); 
+            }
+        });
+    });
+})
+
 
 module.exports = router;
